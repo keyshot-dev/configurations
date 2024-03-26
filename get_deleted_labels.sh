eeb19e66f02cf3e3c343686b624b94df1647fc72
ttn <tobias.nissen@gmail.com>
2024-03-26 14:40:47 +0100

    DAM-7425 Added script for finding deleted labels.

---
+++ get_deleted_labels.sh
@@ -0,0 +1,19 @@
+﻿#!/bin/bash
+
+# Finds the labels that have been removed from one configuration version to another.
+fromVersion=5.8.0
+toVersion=5.10.0
+
+# Temporary files to store label keys
+oldLabelKeys=$(mktemp)
+newLabelKeys=$(mktemp)
+
+# Extract all label keys
+find ./MM/${fromVersion}/config/media_manager_5/labels/ -type f -exec cat {} \; | grep -Eo "key = '(.*)'" | sed -E "s/key = '(.*)'/\1/" | sort -u > "$oldLabelKeys"
+find ./MM/${toVersion}/config/media_manager_5/labels/ -type f -exec cat {} \; | grep -Eo "key = '(.*)'" | sed -E "s/key = '(.*)'/\1/" | sort -u > "$newLabelKeys"
+
+# Print the label keys that have been removed.
+comm -23 "$oldLabelKeys" "$newLabelKeys" | awk '{ printf "\"%s\",\n", $1 } '
+
+# Clean up temporary files
+rm "$oldLabelKeys" "$newLabelKeys"
